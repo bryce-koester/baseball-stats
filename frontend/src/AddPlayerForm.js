dc8979f0f1103b1216c51c6b9fac9548a6342283
Bryce Koester <bryce@thekoesters.org>
2023-03-01 01:51:03 -0500

    added styling and hid add player form when not using

---
+++ frontend/src/AddPlayerForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+
+const AddPlayerForm = ({fetchplayers, setShowAddForm}) => {
+    const [id, setId] = useState('')
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+    const [velocity, setVelocity] = useState('')
+
+    const handleInputsReset = () => {
+        setId('')
+        setName('')
+        setAge('')
+        setVelocity('')
+    }
+    // Create new player
+    const handleplayerCreate = () => {
+        // Send POST request to 'players/create' endpoint
+        axios
+            .post('http://localhost:5000/players/create', {
+                id: id,
+                name: name,
+                age: age,
+                velocity: velocity
+            })
+            .then(res => {
+                console.log(res.data)
+
+                // Fetch all players to refresh
+                // the players on the Players list
+                fetchplayers()
+            })
+            .catch(error => console.error(`There was an error creating the ${name} player: ${error}`))
+    }
+
+    // Submit new player
+    const handleplayerSubmit = () => {
+        // Check if all fields are filled
+        if (id.length > 0 && name.length > 0 && age.length > 0 && velocity.length > 0) {
+            // Create new player
+            handleplayerCreate(id, name, age, velocity)
+
+            console.info(`player ${name} by ${id} added.`)
+
+            // Reset all input fields
+            handleInputsReset()
+
+            //hide form
+            setShowAddForm(false)
+        }
+    }
+
+  return (
+    <div className="player-list-form">
+        <div className="form-wrapper" onSubmit={handleplayerSubmit}>
+          <div className="form-row">
+            <fieldset>
+              <label className="form-label" htmlFor="name">Enter name:</label>
+              <input className="form-input" type="text" id="name" name="name" value={name} onChange={(e) => setName(e.currentTarget.value)} />
+            </fieldset>
+
+            <fieldset>
+              <label className="form-label" htmlFor="id">Enter id:</label>
+              <input className="form-input" type="text" id="id" name="id" value={id} onChange={(e) => setId(e.currentTarget.value)} />
+            </fieldset>
+          </div>
+
+          <div className="form-row">
+            <fieldset>
+              <label className="form-label" htmlFor="age">Enter Age:</label>
+              <input className="form-input" type="text" id="age" name="age" value={age} onChange={(e) => setAge(e.currentTarget.value)} />
+            </fieldset>
+
+            <fieldset>
+              <label className="form-label" htmlFor="velocity">Enter velocity:</label>
+              <input className="form-input" type="text" id="velocity" name="velocity" value={velocity} onChange={(e) => setVelocity(e.currentTarget.value)} />
+            </fieldset>
+          </div>
+        </div>
+
+        <button onClick={handleplayerSubmit} className="btn btn-add">Add the player</button>
+      </div>
+  )
+}
+
+export default AddPlayerForm
--- frontend/src/Players.js
+++ frontend/src/Players.js
@@ -1,9 +1,10 @@
 // Import deps
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import axios from 'axios'
 
 // Import components
 import PlayersList from './PlayersList'
+import AddPlayerForm from './AddPlayerForm'
 
 // Import styles
 import './styles/Players.css'
@@ -11,17 +12,18 @@ import './styles/Players.css'
 // Create Players component
 const Players = () => {
   // Prepare states
-  const [id, setId] = useState('')
-  const [name, setName] = useState('')
-  const [age, setAge] = useState('')
-  const [velocity, setVelocity] = useState('')
   const [players, setplayers] = useState([])
   const [loading, setLoading] = useState(true)
+  const [showAddForm, setShowAddForm] = useState(false)
 
   // Fetch all players on initial render
   useEffect(() => {
     fetchplayers()
   }, [])
+
+  const wrapperSetShowAddForm = useCallback(val => {
+    setShowAddForm(val)
+  }, [setShowAddForm])
 
   // Fetch all players
   const fetchplayers = async () => {
@@ -36,48 +38,6 @@ const Players = () => {
         setLoading(false)
       })
       .catch(error => console.error(`There was an error retrieving the player list: ${error}`))
-  }
-
-  // Reset all input fields
-  const handleInputsReset = () => {
-    setId('')
-    setName('')
-    setAge('')
-    setVelocity('')
-  }
-
-  // Create new player
-  const handleplayerCreate = () => {
-    // Send POST request to 'players/create' endpoint
-    axios
-      .post('http://localhost:5000/players/create', {
-        id: id,
-        name: name,
-        age: age,
-        velocity: velocity
-      })
-      .then(res => {
-        console.log(res.data)
-
-        // Fetch all players to refresh
-        // the players on the Players list
-        fetchplayers()
-      })
-      .catch(error => console.error(`There was an error creating the ${name} player: ${error}`))
-  }
-
-  // Submit new player
-  const handleplayerSubmit = () => {
-    // Check if all fields are filled
-    if (id.length > 0 && name.length > 0 && age.length > 0 && velocity.length > 0) {
-      // Create new player
-      handleplayerCreate()
-
-      console.info(`player ${name} by ${id} added.`)
-
-      // Reset all input fields
-      handleInputsReset()
-    }
   }
 
   // Remove player
@@ -107,39 +67,20 @@ const Players = () => {
     .catch(error => console.error(`There was an error resetting the player list: ${error}`))
   }
 
+  const handleShowAddForm = () => {
+    setShowAddForm(true)
+  }
+
   return (
     <div className="player-list-wrapper">
-      {/* Form for creating new player */}
-      <div className="player-list-form">
-        <div className="form-wrapper" onSubmit={handleplayerSubmit}>
-          <div className="form-row">
-            <fieldset>
-              <label className="form-label" htmlFor="name">Enter name:</label>
-              <input className="form-input" type="text" id="name" name="name" value={name} onChange={(e) => setName(e.currentTarget.value)} />
-            </fieldset>
-
-            <fieldset>
-              <label className="form-label" htmlFor="id">Enter id:</label>
-              <input className="form-input" type="text" id="id" name="id" value={id} onChange={(e) => setId(e.currentTarget.value)} />
-            </fieldset>
-          </div>
-
-          <div className="form-row">
-            <fieldset>
-              <label className="form-label" htmlFor="age">Enter Age:</label>
-              <input className="form-input" type="text" id="age" name="age" value={age} onChange={(e) => setAge(e.currentTarget.value)} />
-            </fieldset>
-
-            <fieldset>
-              <label className="form-label" htmlFor="velocity">Enter velocity:</label>
-              <input className="form-input" type="text" id="velocity" name="velocity" value={velocity} onChange={(e) => setVelocity(e.currentTarget.value)} />
-            </fieldset>
-          </div>
-        </div>
-
-        <button onClick={handleplayerSubmit} className="btn btn-add">Add the player</button>
-      </div>
-
+      {/* Open add player form */}
+      
+    
+      {/* Render Form for creating new player */}
+      {showAddForm ? 
+      <AddPlayerForm fetchplayers={fetchplayers} setShowAddForm={wrapperSetShowAddForm}></AddPlayerForm> : 
+      <button onClick={handleShowAddForm} className="btn btn-add">Add new player</button>}
+      
       {/* Render Players list component */}
       <PlayersList players={players} loading={loading} handleplayerRemove={handleplayerRemove} />
 
